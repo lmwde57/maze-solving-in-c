810c574459c635b5a2e2324a263f96933fd90912
Lee Minwoo <74181807+lmwde57@users.noreply.github.com>
2020-11-14 11:28:26 +0900

    Update 자동 랜덤미로 구현 후 해결법 선택 프로그램.c

--- 자동 랜덤미로 구현 후 해결법 선택 프로그램.c
+++ 자동 랜덤미로 구현 후 해결법 선택 프로그램.c
@@ -158,7 +158,11 @@ void DFS(int i, int j) {
 void squaredfs() { //사각형으로 미로 출력 + DFS 방법수 출력
 	int a, b;
 	unsigned short text2 = 0x0002; //색상 초록색
-	unsigned short text5 = 0x0005; //색상 자주색
+	//unsigned short text5 = 0x0005; //색상 자주색
+	//unsigned short text3 = 0x0003; //색상 하늘색
+	//unsigned short text6 = 0x0006; //색상 노란색
+	unsigned short text7 = 0x0007; //색상 하얀색
+	//clock_t start = clock(); //DFS로 찾는 미로 시간 시작
 	system("cls"); //이걸 void DFS 안에 있는 squaredfs를 사용함으로써 애니메이션처럼 출력
 	for (a = 0; a < MAX_Y; a++) // a 너비
 	{
@@ -170,16 +174,27 @@ void squaredfs() { //사각형으로 미로 출력 + DFS 방법수 출력
 			}
 			else if (save[a][b] == 0) printf("   ");
 			else if (map[a][b] != -1 || map[a][b] != -2) {// DFS를 통해 길이 숫자로 나오므로 -1을 제외한 (아래주석)
-				setColor(text5);
+				setColor(text7);
 				printf("%3d", save[a][b]); // □와 숫자간격을 맞추기 위해 %3d,                          모든 숫자에 대해서 그대로 출력
 			} //sqaredfs와 squarebfs에서 dfs의 경우는 save배열을 지나간 길의 위치를 몇 번째로 지나갔는 지를 표시
 		}     //따라서 save가 0일때 공백으로 하여 길을 출력하고, bfs의 경우는 map 배열을 그대로 바꿔서 가져다 쓰므로 map[a][b] == 0 으로 사용
 		printf("\n");
 	}
+	//if (save[19][19] != 0) {
+		//clock_t end = clock(); //DFS로 찾는 미로 시간 종료
+		//setColor(text6);
+		//printf("해결 시간: %lf초\n", (double)(end - start) / CLOCKS_PER_SEC); //시간 출력
+
+		//setColor(text3);
+		//printf("     %d", save[19][19]); //squaredfs의 주석과 비슷한 방식으로 save[][]로 출력 DFS로 몇 번째에 찾았는지 출력
+		//setColor(text7);
+		//printf("번째에서 찾았습니다.");
+		//exit(0); //exit(0 or 1);함수로 squaredfs함수 즉시종료 0 = 정상종료, 1 = 비정상종료
+		//save[19][19]가 도착지점이므로 해당 지점 도착시 즉시 종료가능. 재귀호출함수라 시간을 재려면 포인터로 clock_t start = clock();
+	//} // 이 부분을 넘겨주어야 함. 이 부분 주석처리를 지우고 아래 main의 select 2번의 squaredfs아래 부분을 전부 주석처리할 시 즉시 종료 프로그램으로 변경.
 }
 
 void squarebfs();
-
 void display() //처음에 보일 -1과 -2, 0으로 이루어진 미로
 {
 	int x, y;
@@ -211,7 +226,8 @@ void BFS(int x, int y, int k) //너비 우선으로 모든 길 동시 탐색
 void squarebfs() { //사각형으로 미로 출력 + BFS 방법수 출력
 	int a, b;
 	unsigned short text2 = 0x0002; //색상 초록색
-	unsigned short text5 = 0x0005; //색상 자주색
+	//unsigned short text5 = 0x0005; //색상 자주색
+	unsigned short text7 = 0x0007; //색상 하얀색
 	system("cls"); //이걸 void BFS 안에 있는 squarebfs를 사용함으로써 애니메이션처럼 출력
 	for (a = 0; a < MAX_X; a++) // a 너비
 	{
@@ -223,7 +239,7 @@ void squarebfs() { //사각형으로 미로 출력 + BFS 방법수 출력
 			}
 			else if (map[a][b] == 0) printf("  ");
 			else if (map[a][b] != -1 || map[a][b] != -2) { // BFS를 통해 길이 숫자로 나오므로 -1을 제외한 (아래주석)
-				setColor(text5);
+				setColor(text7);
 				printf("%2d", map[a][b]); // □와 숫자간격을 맞추기 위해 %2d,                             모든 숫자에 대해서 그대로 출력
 			}
 		}
@@ -245,9 +261,10 @@ int main() {
 	cInfo.dwSize = 10; //커서 크기 조정 관련 10~100 유효값
 	cInfo.bVisible = FALSE; // TRUE 혹은 FALSE 로 커서의 유무 조절 가능
 	SetConsoleCursorInfo(hOut, &cInfo);
+	//커서를 FALSE로 껐으므로 깜빡임이 없으나 콘솔창을 확대하는 순간 커서가 생성됨. (시스템 자체이므로 절대적 수정불가)
 
 	unsigned short text3 = 0x0003; //색상 하늘색
-	unsigned short text6 = 0x0006; //색상 하얀색
+	unsigned short text6 = 0x0006; //색상 노란색
 	unsigned short text7 = 0x0007; //색상 하얀색
 
 	initMap();
@@ -257,7 +274,7 @@ int main() {
 	int select2;
 
 	while (1) {
-		printf("1은 재생성, 2는 미로찾기 시작\n");
+		printf("1은 재생성(Beta), 2는 미로찾기 시작\n");
 		scanf_s("%d", &select2);
 		if (select2 == 1) {
 			system("cls");
@@ -329,6 +346,7 @@ int main() {
 				printf("     %d", save[19][19]); //squaredfs의 주석과 비슷한 방식으로 save[][]로 출력 DFS로 몇 번째에 찾았는지 출력
 				setColor(text7);
 				printf("번째에서 찾았습니다.");
+				exit(0);
 
 				return 0;
 			}
@@ -351,6 +369,8 @@ int main() {
 
 				return 0;
 			}
+			else printf("잘못 입력하셨습니다.\n");
 		}
+		else printf("잘못 입력하셨습니다.\n");
 	}
 }
